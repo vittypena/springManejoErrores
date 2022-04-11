6c6fdb5479999a4fe99286ec74d85896c276cf45
Victor Pena <victorpenawow@gmail.com>
2022-04-11 14:04:46 +0200

    Commit archivo grandes

---
+++ spring-boot-error/src/main/java/com/bolsadeideas/springboot/error/app/sevice/usuarioService.java
@@ -0,0 +1,14 @@
+package com.bolsadeideas.springboot.error.app.sevice;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.bolsadeideas.springboot.error.app.models.domain.Usuario;
+
+public interface usuarioService {
+	
+	public List<Usuario> listar();
+	public Usuario obtenerPorId(Integer id);
+	public Optional<Usuario> obtenerPorIdOptional(Integer id);
+	
+}
